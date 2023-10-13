638f98f2169557125b69ceee83468fe91b293dc8
Tornado-8 <tornado-8@Admin-8s-iMac.local>
2023-10-13 14:16:53 +0800

    sd

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-var input = document.getElementById("enteredValue")
-// const addTask = document.getElementById("addTask");
+const btn = document.getElementById('btn');
+const enteredValue = document.getElementById('userVal');
+let addList = document.getElementById('addList');
 
-input.addEventListener("onchange", updateValue)
 
-function updateValue(e) {
-  addTask.textContent = e.target.value;
-    console.log(e.target.value);
+function handleClick(e) {
+
+  if(enteredValue.value === '') {
+    alert('Hey Dude, WRITE SOMETHING!!!')
+  } else {
+    console.log(enteredValue.value);
+  e.preventDefault();
+  const node = document.createElement("li")
+  const textnode = document.createTextNode(enteredValue.value)
+  node.appendChild(textnode);
+  document.getElementById("addList").appendChild(node);
+  console.log(addList);
+  enteredValue.value = '';
 }
+}
+
+btn.addEventListener('click', handleClick);
+/* btn.addEventListener('keypress', handleClick(e)
+{
+  if (e.keypress === 'Enter'){
+    handleClick(e)
+  }
+});
+ */
