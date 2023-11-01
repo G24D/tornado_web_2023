0e3a9b05b0b06820baa79da5734163e33e01265f
Tornado-8 <tornado-8@Admin-8s-iMac.local>
2023-11-01 17:32:52 +0800

    hehe

---
+++ lesson/lesson_day_18/lesson_18_dom.js
@@ -0,0 +1,9 @@
+console.log('Lesson Day 18 - DOM Scripts');
+
+var aboutMe = 'My name is AG';
+
+var content = document.getElementById('content');
+
+console.log(content);
+
+content.innerHTML = aboutMe;
