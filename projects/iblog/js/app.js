1079afa259af8a1e7376697c01279c19d7137371
G24D <ssktt.aye@gmail.com>
2023-11-03 21:25:08 +0800

    'hehe'

--- projects/iblog/js/app.js
+++ projects/iblog/js/app.js
@@ -1,54 +1,3 @@
-/* const pinnedContainer = document.querySelector('.pinned-posts')
-const pinnedPosts = [
-    {
-        title: 'Welcome to Web Development',
-        postedBy: 'Tilo Mitra',
-        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iste magnam consectetur quo minima necessitatibus voluptas, consequatur facilis asperiores aperiam, magni repudiandae blanditiis commodi modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iste magnam consectetur quo minima necessitatibus voluptas, consequatur facilis asperiores aperiam, magni repudiandae blanditiis commodi modi.',
-    },
-]
-const postMethods = () => {
-
-    pinnedPosts.forEach((pinnedPost) => {
-        const postEl = document.createElement('div')
-        postEl.classList.add('posts');
-        postEl.innerHTML =
-            `<h1>${pinnedPost.title}</h1>
-            <i>By <a href="#">${pinnedPost.postedBy}</a> under 
-               <button class="btn1">css</button> 
-               <button class="btn2">html</button> 
-            </i>
-            <img src="./img/pic-1.jpg" alt="" width="60px">
-            <div style="margin-top: 20px;">${pinnedPost.info}</div>
-            `
-        pinnedContainer.appendChild(postEl);
-        const btn = document.getElementById('btn4blog')
-        const enteredName = document.getElementById('fname')
-        const enteredBlog = document.getElementById('bloginfo')
-
-        const addBlogs = (e) => {
-            if(enteredName.value === '' && enteredBlog.value === '') 
-            { 
-            alert('Bro write something 1st') 
-            }
-            else 
-            {
-                console.log(enteredName.value);
-                console.log(enteredBlog.value);
-                pinnedPosts.push(enteredName.value);
-                pinnedPosts.push(enteredBlog.value);
-                e.preventDefault();
-
-            }
-
-        }
-        btn.addEventListener("click", addBlogs());
-})
-        }
-
-postMethods();
-
-
- */
 
 const pinnedContainer = document.querySelector('.pinned-posts');
 const pinnedPosts = [
@@ -77,15 +26,47 @@ const postMethods = () => {
             postEl.classList.add('posts');
             postEl.innerHTML =
                 `<h1>${pinnedPost.title}</h1>
-                <i>By <a href="#">${pinnedPost.postedBy}</a>
-                <button class="btn1">css</button> 
+                <i>By&nbsp&nbsp<a href="#">${pinnedPost.postedBy}</a>
+                &nbsp&nbsp <span>under</span> &nbsp&nbsp<button class="btn1">css</button> &nbsp&nbsp 
                 <button class="btn2">html</button> 
-                </i>
                 <img src="./img/pic-1.jpg" alt="" width="60px">
-                <div style="margin-top: 20px;">${pinnedPost.info}</div>`;
+                </i>
+                <div style="margin-top: 20px;">${pinnedPost.info}</div>
+                
+                `;
             pinnedContainer.appendChild(postEl);
         });
     }
+
+const recentContainer = document.querySelector('.recent-posts');
+const recentPosts = [
+    {
+        title: 'Everything You Need to Know About ReactJS',
+        postedBy: 'Eric Ferraiuolo',
+        info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iste magnam consectetur quo minima necessitatibus voluptas, consequatur facilis asperiores aperiam, magni repudiandae blanditiis commodi modi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iste magnam consectetur quo minima necessitatibus voluptas, consequatur facilis asperiores aperiam, magni repudiandae blanditiis commodi modi.',
+    }
+];
+ 
+    
+    function displayRecentPosts() {
+        recentContainer.innerHTML = '';
+        recentPosts.forEach((recentPost) => {
+            const postEl = document.createElement('div');
+            postEl.classList.add('posts');
+            postEl.innerHTML =
+        `   <h1>${recentPost.title}</h1>
+            <i>By&nbsp&nbsp
+                <a href="#">${recentPost.postedBy}</a>
+                &nbsp&nbsp
+                <span>under</span>&nbsp&nbsp
+                <button class="btn3">JavaScript</button> &nbsp&nbsp
+                <img src="./img/pic-1.jpg" alt="" width="60px">
+                
+            </i>
+            <div>${recentPost.info}</div>`;
+        recentContainer.appendChild(postEl);
+    })
+};
 
     const btn = document.getElementById('btn4blog');
     const enteredName = document.getElementById('fname');
@@ -106,6 +87,10 @@ const postMethods = () => {
 
     btn.addEventListener("click", addBlogs);
     displayPinnedPosts();
+    displayRecentPosts();
 }
 
+const selectBlog = document.querySelectorAll('.selectBlog')
+
+console.log(selectBlog);
 postMethods();
